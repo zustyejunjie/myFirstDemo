30bbaaacfcc12c7e12cb12cbfc17174d776d9602
yejunjie <88888888>
2016-09-09 09:18:07 +0800

    标准web项目框架

--- spring-web/src/main/java/net/xuele/controller/MainController.java
+++ spring-web/src/main/java/net/xuele/controller/MainController.java
@@ -1,5 +1,7 @@
 package net.xuele.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -11,6 +13,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
 @RequestMapping(value = "main")
 public class MainController {
 
+    private Logger logger = LoggerFactory.getLogger(MainController.class);
+
     @RequestMapping(value = "/",method = RequestMethod.GET)
     public String index() {
         return "index";
@@ -19,6 +23,7 @@ public class MainController {
 
     @RequestMapping(value = "getPage1",method = RequestMethod.GET)
     public String getPage1() {
+        logger.info("get into page one");
         return "page1";
     }
 }
--- spring-web/src/main/webapp/index.jsp
+++ spring-web/src/main/webapp/index.jsp
@@ -1,6 +1,6 @@
 <html>
 <body>
 <h2>Hello World! </h2>
-<h1>1231</h1>
+<h1>this is index page</h1>
 </body>
 </html>
