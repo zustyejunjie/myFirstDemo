f87685d7a1708ab34e563124f43efd0dc5823841
yejunjie <zustyejunjie@163.com>
2016-12-22 20:39:45 +0800

    first commit

---
+++ spring-service/src/main/java/net/xuele/aes/gitTest/GitTest.java
@@ -0,0 +1,11 @@
+package net.xuele.aes.gitTest;
+
+/**
+ * Created by yejunjie on 2016/12/22.
+ */
+public class GitTest {
+
+    public static void main(String[] args) {
+        String a = "aa";
+    }
+}
