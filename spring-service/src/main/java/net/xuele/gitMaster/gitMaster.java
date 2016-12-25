406f0bfd013fb8d77e2862b5887c3ca6ddbda64d
yejunjie <zustyejunjie@163.com>
2016-12-25 15:05:50 +0800

    git master commit

---
+++ spring-service/src/main/java/net/xuele/gitMaster/gitMaster.java
@@ -0,0 +1,12 @@
+package net.xuele.gitMaster;
+
+/**
+ * Created by yejunjie on 2016/12/25.
+ */
+public class gitMaster {
+
+
+    public static void main(String[] args) {
+        System.out.print("git master");
+    }
+}
